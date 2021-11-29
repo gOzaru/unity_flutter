239b157d3c55ae1174fffc54a7203bea22663810
William Gosal <gOzaru@users.noreply.github.com>
2021-11-29 08:39:56 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'screens/orientation_screen.dart';
+import 'menu_screen.dart';
+import 'screens/api_screen.dart';
+import 'screens/loader_screen.dart';
+import 'screens/simple_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Unity Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MenuScreen(),
+        '/simple': (context) => const SimpleScreen(),
+        '/loader': (context) => const LoaderScreen(),
+        '/orientation': (context) => const OrientationScreen(),
+        '/api': (context) => const ApiScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/simple_screen.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
+
+class SimpleScreen extends StatefulWidget {
+  const SimpleScreen({Key? key}) : super(key: key);
+
+  @override
+  _SimpleScreenState createState() => _SimpleScreenState();
+}
+
+class _SimpleScreenState extends State<SimpleScreen> {
+  static final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  late UnityWidgetController _unityWidgetController;
+  double _sliderValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _unityWidgetController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Simple Screen'),
+      ),
+      body: Card(
+          margin: const EdgeInsets.all(8),
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Stack(
+            children: [
+              UnityWidget(
+                borderRadius: BorderRadius.zero,
+                onUnityCreated: _onUnityCreated,
+                onUnityMessage: onUnityMessage,
+                onUnitySceneLoaded: onUnitySceneLoaded,
+              ),
+              Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Card(
+                  elevation: 10,
+                  child: Column(
+                    children: <Widget>[
+                      const Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Text("Rotation speed:"),
+                      ),
+                      Slider(
+                        onChanged: (value) {
+                          setState(() {
+                            _sliderValue = value;
+                          });
+                          setRotationSpeed(value.toString());
+                        },
+                        value: _sliderValue,
+                        min: 0,
+                        max: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+
+  void setRotationSpeed(String speed) {
+    _unityWidgetController.postMessage(
+      'Cube',
+      'SetRotationSpeed',
+      speed,
+    );
+  }
+
+  void onUnityMessage(message) {
+    print('Received message from unity: ${message.toString()}');
+  }
+
+  void onUnitySceneLoaded(SceneLoaded? scene) {
+    print('Received scene loaded from unity: ${scene!.name}');
+    print('Received scene loaded from unity buildIndex: ${scene.buildIndex}');
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void _onUnityCreated(controller) {
+    _unityWidgetController = controller;
+  }
+}
